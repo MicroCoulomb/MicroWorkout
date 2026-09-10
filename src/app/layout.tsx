8aee413479374f82d7b4bb6cb1bc2e2ae51c9829
MicroCoulomb <rbpalmiano@gmail.com>
2026-09-11 03:15:59 +0800

    added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,13 @@ export const metadata: Metadata = {
   title: "MicroWorkout",
   description: "Plan less. Move more. Track every set.",
   applicationName: "MicroWorkout",
+  icons: {
+    icon: [
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/icon.png", type: "image/png" }],
+  },
   appleWebApp: { capable: true, statusBarStyle: "black-translucent", title: "MicroWorkout" },
 };
 
